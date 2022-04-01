86a30f38e6dc35167903963242ad41e82c1d9ed2
jhonacs2 <jhonrocker2012@gmail.com>
2022-04-01 07:55:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-vgb86m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-vgb86m)
---
+++ index.ts
@@ -0,0 +1,8 @@
+console.log('Hola mundos');
+
+const a: number = 10;
+
+console.log(a);
+
+// IMPORTANTE:
+// Ver la consola de JavaScript
